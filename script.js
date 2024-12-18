aa38e02156df99b7a358cd699329c8467ba9ed6e
Salvatore <workstudioms@gmail.com>
2024-12-18 13:17:32 +0100

    js-fizzbuzz , code readability, fix

--- script.js
+++ script.js
@@ -22,37 +22,42 @@
 // Il valore iniziale assegnato alla variabile i è 1.
 // La condizione prevede che i sia minore o uguale a 100.
 // Alla fine di ogni iterazione, il valore di i viene incrementato di 1.
+// Dichiaro una variabile "messaggio", per migliorare la leggibilità del codice.
 
 
 
 for (let i = 1; i <= 100; i++) {
 
+    let messaggio;
+
     // condizione 1: il numero è sia multiplo di 3 che di 5
     if (i % 3 === 0 && i % 5 === 0) {
 
-        console.log("FizzBuzz");
+        messaggio = "FizzBuzz";
 
     }
 
     // condizione 2: il numero è multiplo di 3
     else if (i % 3 === 0) {
 
-        console.log("Fizz");
+        messaggio = "Fizz";
 
     }
 
     // condizione 3: il numero è multiplo di 5
     else if (i % 5 === 0) {
 
-        console.log("Buzz");
+        messaggio = "Buzz";
 
     }
 
     // il numero non rientra nelle condizioni precedenti
     else {
 
-        console.log(i);
+        messaggio = i;
 
     }
+
+    console.log(messaggio);
 
 }
